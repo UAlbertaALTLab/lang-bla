8860f50b0a45b0f629918ae7f3c7cc6e5f0014b6
aarppe <antti.arppe@iki.fi>
2026-05-19 23:41:10 -0600

    Created script for converting TSV file with alternative stem information (created by Katie) into LEXC source code.

---
+++ fst/morphology/scripts/bla_nouns_tsv2lexc.sh
@@ -0,0 +1,60 @@
+#!/bin/sh
+
+# To create FST stems for the “initial” list”:
+# •	[column 6][ column 1 ]:[column 6][column 2]\t[column 5] ;
+# To create FST stems for the “person” list:
+# •	[column 6][ column 1 ]:[column 6][column 3]\t[column 5] ;
+# To create FST stems for the “elsewhere” list:
+# •	[column 6][ column 1 ]:[column 6][column 4]\t[column 5] ;
+
+
+gawk -F"\t" '{
+  stem=$1;
+  initial=$2;
+  person=$3;
+  elsewhere=$4;
+  contlex=$5;
+  flags=$6;
+  notes=$7;
+
+  if(match(contlex, "NA")!=0)
+    pos="NA";
+  if(match(contlex, "NI")!=0)
+    pos="NI";
+
+  if(notes!="")
+    initlexc=sprintf("%s%s:%s%s %s ; ! %s\n", flags, stem, flags, initial, contlex, notes);
+  else
+    initlexc=sprintf("%s%s:%s%s %s ;\n", flags, stem, flags, initial, contlex);
+  initstems[pos]=initstems[pos] initlexc;
+
+  if(notes!="")
+    personlexc=sprintf("%s%s:%s%s %s ; ! %s\n", flags, stem, flags, initial, contlex, notes);
+  else
+    personlexc=sprintf("%s%s:%s%s %s ;\n", flags, stem, flags, initial, contlex);
+  personstems[pos]=personstems[pos] elselexc;
+
+  if(notes!="")
+    elselexc=sprintf("%s%s:%s%s %s ; ! %s\n", flags, stem, flags, initial, contlex, notes);
+  else
+    elselexc=sprintf("%s%s:%s%s %s ;\n", flags, stem, flags, initial, contlex);
+  elsestems[pos]=elsestems[pos] elselexc;
+
+}
+END {
+  for(p in initstems)
+     {
+       printf "LEXICON INIT_%s_STEMS\n", p;
+       printf "%s\n", initstems[p];
+     }
+  for(p in personstems)
+     {
+       printf "LEXICON INIT_%s_STEMS\n", p;
+       printf "%s\n", personstems[p];
+     }
+  for(p in elsestems)
+     {
+       printf "LEXICON INIT_%s_STEMS\n", p;
+       printf "%s\n", elsestems[p];
+     }
+}'
